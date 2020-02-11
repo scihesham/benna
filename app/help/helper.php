6b5695f18dab2a0d9459d5238aeae68b7d776461
Hesham Samir <scihesham12@gmail.com>
2020-02-11 17:22:00 +0200

    edit notification system

--- app/help/helper.php
+++ app/help/helper.php
@@ -65,22 +65,30 @@ function ksaCities(){
 function messagesNotification(){
     
     $user_id = Auth::user()->id;
-    $messages = collect(\App\Message::whereRaw("id IN (select max(`id`) from messages where to_user=$user_id or send_from=$user_id GROUP BY offer_status_id)")->orderBy('id', 'desc')->limit(4)->get());
+    $messages = collect(\App\Message::whereRaw("id IN (select max(`id`) from messages where to_user=$user_id or send_from=$user_id GROUP BY offer_status_id)")->orderBy('id', 'desc')->limit(15)->get());
         
-    $dispute = collect(\App\DisputeMessage::orderBy('id', 'desc')->limit(4)->get());
+    $dispute = collect(\App\DisputeMessage::orderBy('id', 'desc')->limit(15)->get());
     $all_messages = $dispute->merge($messages)->sortByDesc('created_at');
  
     return $all_messages;   
 }
 
 function awardProjects(){
-    $projects = \App\Project::where([['company_id', Auth::user()->id], ['award_offer_seen', '0']])->get();
+    $projects = \App\Project::where([['company_id', Auth::user()->id], ['status', 1]])->get();
+    foreach($projects as $project){
+       $project['type'] = 'award project';
+    }
+    return $projects; 
+}
+
+function awardProjects_not_seen(){
+    $projects = \App\Project::where([['company_id', Auth::user()->id], ['status', 1], ['award_offer_seen', '0']])->get();
     return $projects; 
 }
 
 /* to show the projects  that ends */
 function endProjects(){
-    $projects = \App\Project::where([['company_id', Auth::user()->id],  ['status', 2]])->orderBy('project_end_at', 'desc')->limit(5)->get();
+    $projects = \App\Project::where([['company_id', Auth::user()->id],  ['status', 2]])->orderBy('project_end_at', 'desc')->limit(15)->get();
     
     foreach($projects as $project){
        $project['type'] = 'end project';
@@ -91,7 +99,7 @@ function endProjects(){
 
 /* to count the projects  that ends and not seen */
 function endProjects_not_seen(){
-    $projects = \App\Project::where([['company_id', Auth::user()->id],  ['status', 2], ['project_end_at_seen', 0]])->orderBy('project_end_at', 'desc')->limit(5)->get();
+    $projects = \App\Project::where([['company_id', Auth::user()->id],  ['status', 2], ['project_end_at_seen', 0]])->orderBy('project_end_at', 'desc')->limit(15)->get();
 
     return $projects;
 }
@@ -109,7 +117,7 @@ function evaluationNotification(){
      $evaluations = App\Evaluation::where([[$rating_type, '!=', '']])
          ->whereHas('project', function ($query) use($user_type) {
              $query->where($user_type, Auth::user()->id);
-         })->orderBy($time_type, 'desc')->limit(5)->get(); 
+         })->orderBy($time_type, 'desc')->limit(10)->get(); 
     
     foreach($evaluations as $evaluation){
         $evaluation['created_at'] = $evaluation->$time_type;
@@ -133,7 +141,7 @@ function evaluationNotification_not_seen(){
      $evaluations = App\Evaluation::where([[$rating_type, '!=', ''], [$seen_type, 0]])
          ->whereHas('project', function ($query) use($user_type) {
              $query->where($user_type, Auth::user()->id);
-         })->orderBy($time_type, 'desc')->limit(5)->get(); 
+         })->orderBy($time_type, 'desc')->limit(10)->get(); 
     
     foreach($evaluations as $evaluation){
         $evaluation['created_at'] = $evaluation->$time_type;
@@ -145,10 +153,34 @@ function evaluationNotification_not_seen(){
 
 function overdateInvoices(){
     $res = [];
+    $invoices = \App\Invoice::where([['status', 0]])
+                ->whereHas('offer', function ($query) {
+                    $query->where('company_id',  Auth::user()->id);
+               })->orderBy('id', 'desc')->get();
+    
+    $now = strval(date('y-m-d', time())); // or your date as well
+    $now = strtotime($now);
+    
+    foreach($invoices as $key => $invoice){
+        $your_date = strtotime($invoice->created_at->format('Y-m-d'). ' + 8 days');
+        $datediff = $your_date - $now;
+        $days_num = round($datediff / (60 * 60 * 24));
+        if($days_num < 0){
+           $invoice['days_num'] = $days_num;
+           $res[] =  $invoice;
+        }
+    }
+    
+    return $res;
+}
+
+
+function overdateInvoicesUnseen(){    
+    $res = [];
     $invoices = \App\Invoice::where([['status', 0], ['seen_overdate_invoice', 0]])
                 ->whereHas('offer', function ($query) {
                     $query->where('company_id',  Auth::user()->id);
-               })->orderBy('id', 'asc')->get();
+               })->orderBy('id', 'desc')->get();
     
     $now = strval(date('y-m-d', time())); // or your date as well
     $now = strtotime($now);
@@ -186,19 +218,31 @@ function last_overdateInvoices_general(){
     return $res_id;    
 }
 
+function user_invoices(){
+        $invoices = collect(\App\Invoice::whereHas('offer', function ($query) {
+                    $query->where('company_id',  Auth::user()->id);
+               })->orderBy('id', 'desc')->limit(15)->get()
+            );
 
+    
+    $all_notifications = $invoices->merge(overdateInvoices())->sortByDesc('created_at');
+    
+    return $all_notifications;
+}
 /* freelancer invoices & projects notification */
 function invoices_projects(){
-    $projects = collect(\App\Project::where('status', '0')->orderBy('id', 'desc')->limit(5)->get());
-    $invoices = collect(\App\Invoice::whereHas('offer', function ($query) {
-                    $query->where('company_id',  Auth::user()->id);
-               })->orderBy('id', 'desc')->limit(5)->get()
-            );
+    $projects = collect(\App\Project::where('status', '0')->orderBy('id', 'desc')->limit(15)->get());
+//    $invoices = collect(\App\Invoice::whereHas('offer', function ($query) {
+//                    $query->where('company_id',  Auth::user()->id);
+//               })->orderBy('id', 'desc')->limit(10)->get()
+//            );
     $end_projects = collect(endProjects());
     
     $rating = collect(evaluationNotification());
     
-    $all_notifications = $projects->merge($end_projects)->merge($invoices)->merge($rating)->sortByDesc('created_at');
+//    $all_notifications = $projects->merge($end_projects)->merge($invoices)->merge($rating)->sortByDesc('created_at');
+    
+    $all_notifications = $projects->merge($end_projects)->merge(awardProjects())->merge($rating)->sortByDesc('created_at');
 
     return $all_notifications;
 }
@@ -206,7 +250,7 @@ function invoices_projects(){
 
 /* owner offer notification */
 function offerNotification(){
-   $offer =  collect(\App\OfferStatus::where('owner_id', Auth::user()->id)->orderBy('id', 'desc')->limit(4)->get()); 
+   $offer =  collect(\App\OfferStatus::where('owner_id', Auth::user()->id)->orderBy('id', 'desc')->limit(10)->get()); 
     
    $rating = collect(evaluationNotification());
     
@@ -219,7 +263,7 @@ function offerNotification(){
 function admin_overdateInvoices(){
     $res = [];
     $invoices = \App\Invoice::where([['status', 0]])
-                ->orderBy('id', 'asc')->limit(10)->get();
+                ->orderBy('id', 'desc')->get();
     
     $now = strval(date('y-m-d', time())); 
     $now = strtotime($now);
@@ -241,10 +285,11 @@ function admin_overdateInvoices(){
     
 /* admin invoices notification */
 function invoicesNotification(){
-    $receipt = collect(\App\Receipt::orderBy('id', 'desc')->limit(10)->get());  
-    $invoice = collect(\App\Invoice::where('status', '0')->orderBy('id', 'desc')->limit(10)->get());
-    $overdateInvoices = collect(admin_overdateInvoices());
-    $all_invoices = $receipt->merge($overdateInvoices)->merge($invoice)->sortByDesc('created_at');
+    $receipt = collect(\App\Receipt::orderBy('id', 'desc')->limit(15)->get());  
+    $invoice = collect(\App\Invoice::where('status', '0')->orderBy('id', 'desc')->limit(15)->get());
+//    $overdateInvoices = collect(admin_overdateInvoices());
+//    $all_invoices = $receipt->merge($overdateInvoices)->merge($invoice)->sortByDesc('created_at');
+    $all_invoices = $receipt->merge($invoice)->sortByDesc('created_at');
  
     return $all_invoices;   
 }
